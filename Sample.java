ad14e0c83756b458d36cab28606002cf34a8bedd
kushi2022 <gayatri.nitya9@gmail.com>
2024-10-29 11:56:08 +0530

    task_1 is added

---
+++ Sample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Sample {
+
+
+
+    public static void main(String[] args) {
+        //int c=add();
+        int a=2;
+        int b=3;
+        int c=a+b;
+
+        System.out.println("sum of two numbers = "+c);
+    }
+
+
+}
