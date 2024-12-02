035434891ba903e1b0001250f4ed332732b94a95
kushi2022 <gayatri.nitya9@gmail.com>
2024-12-02 18:19:10 +0530

    added group tasks---->2

--- Main.java
+++ Main.java
@@ -1,9 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello Janu welcome to IT world");
+class Grandfather {
+    String land = "Land (Grandfather's property)";
+    public void showLand(){
+        System.out.println("Inherited property: " + land);
     }
+}
 
+class Father extends Grandfather {
+    String house = "House (Father's property)";
+
+    public void showHouse() {
+        System.out.println("Father's own property: " + house);
+    }
+}
+
+class Son extends Father {
+    int bankBalance = 500000;
+
+    public void showBankbalance() {
+        System.out.println("Son's bank balance: " + bankBalance);
+
+    }
+}
+
+
+class Grandson extends Son {
+    String car = "Car (Grandson's property)";
+
+    public void showCar() {
+        System.out.println("Grandson's property: " + car);
+    }
+}
+
+class Greatgrandson extends Grandson {
+    String gold = "Gold (Greatgrandson's property)";
+
+    public void showGold() {
+        System.out.println("Greatgrandson's property: " + gold);
+    }
+}
+
+public class Main{
+
+    public static void main(String[] args) {
+        Greatgrandson greatgrandson = new Greatgrandson();
+        greatgrandson.showLand();
+        greatgrandson.showHouse();
+        greatgrandson.showBankbalance();
+        greatgrandson.showCar();
+        greatgrandson.showGold();
+
+        System.out.println("*Properties list**");
+        System.out.println("---> :" + greatgrandson.land);
+        System.out.println("---> :" + greatgrandson.house);
+        System.out.println("---> :" + greatgrandson.bankBalance);
+        System.out.println("---> :" + greatgrandson.car);
+        System.out.println("---> :" + greatgrandson.gold);
+    }
 }
